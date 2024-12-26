c7f41cd3b43f77f789a352941957e1648add2c6c
Nedim Mazalovic <nedim.mazalovic97@gmail.com>
2024-12-26 17:13:25 +0100

    finalna verzija

---
+++ zadatak4.js
@@ -0,0 +1,23 @@
+/*Ulaz je string koji se sastoji od riječi i razmaka. Vratiti dužinu posljednje riječi u stringu.
+
+Riječ je maksimalni podstring koji se sastoji samo od znakova koji nisu razmaci.
+
+Primjer 1:
+
+Ulaz: str = "Hello World"
+Izlaz: 5
+Objašnjenje: Posljednja riječ je "World" čija je dužina 5.
+
+
+Primjer 2:
+
+Ulaz: s = " fly me to the moon   "
+Izlaz: 4
+Objašnjenje: Posljednja riječ je "moon" čija je dužina 4.
+*/
+
+const string = "Hello World";
+
+const reduced = string.forEach((element) => {});
+
+console.log(reduced);
